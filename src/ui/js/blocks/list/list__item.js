21bb3faafe04159e4e409c7f706775a4ddc3611e
Иван Сабиров <sabirivan@yandex.com>
2023-07-23 19:59:29 +0600

    Update files

--- src/ui/js/blocks/list/list__item.js
+++ src/ui/js/blocks/list/list__item.js
@@ -3,6 +3,31 @@ import * as Render from "../../../../lib/render.js";
 
 database.then(item => item.default.forEach(item => {
     Render.make("li", {
+        append: [
+            ["h2", {
+                append: item.title,
+                attr: { class: "list__title" }
+            }],
+            ["input", {
+                attr: {
+                    type: "checkbox",
+                    id: "checkbox-" + item.id,
+                    class: "field field--checkbox"
+                }
+            }],
+            ["label", {
+                attr: {
+                    for: "checkbox-" + item.id,
+                    class: "list__label"
+                }
+            }],
+            ["button", {
+                append: "Удалить задачу",
+                attr: {
+                    class: "button button--removal"
+                }
+            }]
+        ],
         parent: document.querySelector(".list"),
         attr: { class: "list__item" }
     });
