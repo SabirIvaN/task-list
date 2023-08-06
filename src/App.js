d9e063747fa3a0656a4d9cb073a267deb05ce78d
Иван Сабиров <sabirivan@yandex.com>
2023-08-06 13:01:10 +0600

    Fixed bugs

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import * as wrap from "./ui/blocks/wrap/wrap.js";          // Importing the wrap component
+import * as app from "./ui/blocks/app/app.js";             // Importing the app component
+import * as header from "./ui/blocks/header/header.js";    // Importing the header component
+import * as main from "./ui/blocks/main/main.js";          // Importing the main component
+import * as list from "./ui/blocks/list/list.js";          // Importing the list component
+import * as listItem from "./ui/blocks/item/item.js";      // Importing the item component
--- src/ui/scripts.js
+++
@@ -1,6 +0,0 @@
-import * as wrap from "./blocks/wrap/wrap.js";          // Importing the wrap component
-import * as app from "./blocks/app/app.js";             // Importing the app component
-import * as header from "./blocks/header/header.js";    // Importing the header component
-import * as main from "./blocks/main/main.js";          // Importing the main component
-import * as list from "./blocks/list/list.js";          // Importing the list component
-import * as listItem from "./blocks/item/item.js";      // Importing the item component
